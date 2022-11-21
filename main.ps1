67c3543cfb41be6d220e255a97317df1b038f8ae
humbertocrispim <humberto.cunha.crispim@gmail.com>
2022-11-21 17:33:13 -0300

    fff

--- main.ps1
+++ main.ps1
@@ -7,8 +7,10 @@
 # Enable run script PowerShell
 powershell -command "& { Set-ExecutionPolicy Bypass }"
 
-powershell -command "& { iwr https://github.com/humbertocrispim/automate_program_installation/archive/refs/heads/main.zip -OutFile C:\ficha_contrato.zip }"
+powershell -command "& { iwr https://github.com/humbertocrispim/FICHAS-DE-CONTRATO/archive/refs/heads/main.zip -OutFile C:\ficha_contrato.zip }"
 powershell -command "& { Expand-Archive -Path C:\ficha_contrato.zip -DestinationPath C:\ }"
+powershell -command "& { Expand-Archive -Path C:\FICHAS DE CONTRATO.zip -DestinationPath C:\ }"
+
 
 # Disable run script PowerShell
 powershell -command "& { Set-ExecutionPolicy Default }"
