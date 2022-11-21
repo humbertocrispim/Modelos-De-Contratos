a5e2d539029d71f2782ce5419be9e760d1465bac
humbertocrispim <humberto.cunha.crispim@gmail.com>
2022-11-21 18:05:19 -0300

    wdd

--- _main/main.bat
+++ _main/main.bat
@@ -5,8 +5,7 @@ powershell -command "& { Set-ExecutionPolicy Bypass }"
 powershell -command "& { iwr https://github.com/humbertocrispim/automate_program_installation/archive/refs/heads/main.zip -OutFile C:\ficha_contrato.zip }"
 powershell -command "& { Expand-Archive -Path C:\ficha_contrato.zip -DestinationPath C:\ }"
 powershell -command "& { mv C:\automate_program_installation-main 'C:\Modelos De Contratos' } "
-cd "Modelos De Contratos"
-powershell -command "& { cd 'C:\Modelos De Contrato's } "
+rmdir /s /q "C:\Modelos De Contratos\_main"
 
 
 
