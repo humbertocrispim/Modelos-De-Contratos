357b2d07c7ff60b29ad2c77d01f5c641194ccbfe
humbertocrispim <humberto.cunha.crispim@gmail.com>
2022-11-21 17:33:18 -0300

    ffe

--- main.bat
+++ main.bat
@@ -1,15 +1,9 @@
-##########################################################################################################################################################################
-#                           By: Humberto Cunha                                                                                                                            #
-#                           Git: https://github.com/humbertocrispim                                                                                                       #
-#                                                                                                                                                                         #
-##########################################################################################################################################################################
+echo off
 
-# Enable run script PowerShell
 powershell -command "& { Set-ExecutionPolicy Bypass }"
 
-powershell -command "& { iwr https://github.com/humbertocrispim/automate_program_installation/archive/refs/heads/main.zip -OutFile C:\ficha_contrato.zip }"
+powershell -command "& { iwr https://github.com/humbertocrispim/FICHAS-DE-CONTRATO/archive/refs/heads/main.zip -OutFile C:\ficha_contrato.zip }"
 powershell -command "& { Expand-Archive -Path C:\ficha_contrato.zip -DestinationPath C:\ }"
 
-# Disable run script PowerShell
 powershell -command "& { Set-ExecutionPolicy Default }"
 
