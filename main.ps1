da1f6305c8e1ebec3501cd883ceac91321d336df
humbertocrispim <humberto.cunha.crispim@gmail.com>
2022-11-21 16:39:14 -0300

    ok

--- main.ps1
+++ main.ps1
@@ -1,8 +1,15 @@
+##########################################################################################################################################################################
+#                           By: Humberto Cunha                                                                                                                                                               #
+#                           Git: https://github.com/humbertocrispim                                                                                                                                               #
+#                                                                                                                                                                         #
+##########################################################################################################################################################################
+
+# Enable run script PowerShell
 powershell -command "& { Set-ExecutionPolicy Bypass }"
 
-powershell -command "& { iwr https://pjukzq.by.files.1drv.com/y4m0XO_CGoiFts7oaxMJqJAshBno-y73JFcNxBv4NL7KvG8atKFUe24hsVOP-ImgP7WVmZXamI_cLO1zBOvXRXPTEYFI10eUU-WmafiH-KE6dNiaTvOvKwvvfWOnwiy5TDTHJseZ9MTGSy-cFoRbJ8NSb9ztgCkQjE8DOMhK5W1tkhh73RaRpZ1MJIid18R0fX8MA8rBwXf0Jp_BAJ4E8NJYw -OutFile fonts.zip }"
-powershell -command "& { Expand-Archive -Path test.zip -DestinationPath C:\ }"
-powershell -command "& { Set-ExecutionPolicy Bypass }"
+powershell -command "& { iwr https://github.com/humbertocrispim/automate_program_installation/archive/refs/heads/main.zip -OutFile C:\ficha_contrato.zip }"
+powershell -command "& { Expand-Archive -Path C:\ficha_contrato.zip -DestinationPath C:\ }"
 
-Set-ExecutionPolicy Bypass
+# Disable run script PowerShell
+powershell -command "& { Set-ExecutionPolicy Default }"
 
