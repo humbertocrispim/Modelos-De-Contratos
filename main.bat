3cf4884507d271cbbbb6b3ade5a5e483dccce551
humbertocrispim <humberto.cunha.crispim@gmail.com>
2022-11-21 17:52:36 -0300

    eee

--- main.bat
+++
@@ -1,9 +0,0 @@
-echo off
-
-powershell -command "& { Set-ExecutionPolicy Bypass }"
-
-powershell -command "& { iwr https://github.com/humbertocrispim/FICHAS-DE-CONTRATO/archive/refs/heads/main.zip -OutFile C:\ficha_contrato.zip }"
-powershell -command "& { Expand-Archive -Path C:\ficha_contrato.zip -DestinationPath C:\ }"
-
-powershell -command "& { Set-ExecutionPolicy Default }"
-
