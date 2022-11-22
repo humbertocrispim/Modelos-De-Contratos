71baf2a832559a57ff79ce0cecdabd96a39a202e
humbertocrispim <humberto.cunha.crispim@gmail.com>
2022-11-22 19:49:41 -0300

    ss

--- .main/main.ps1
+++ .main/main.ps1
@@ -1,19 +1,27 @@
 ##########################################################################################################################################################################
-#                           By: Humberto Cunha                                                                                                                            #
+#                           By: Humberto Crispim                                                                                                                          #
 #                           Git: https://github.com/humbertocrispim                                                                                                       #
 #                                                                                                                                                                         #
 ##########################################################################################################################################################################
 
-echo off
 
-# Enable run script PowerShell
-powershell -command "& { Set-ExecutionPolicy Bypass }"
+$PathDst = "C:\Modelos-De-Contratos" 
 
-powershell -command "& { iwr https://github.com/humbertocrispim/automate_program_installation/archive/refs/heads/main.zip -OutFile C:\ficha_contrato.zip }"
-powershell -command "& { Expand-Archive -Path C:\ficha_contrato.zip -DestinationPath C:\ }"
-powershell -command "& { mv C:\automate_program_installation-main 'C:\Modelos De Contratos' } "
-rmdir /s /q "C:\Modelos De Contratos\_main"
-rmdir /s /q C:\ficha_contrato.zip
+if (-not(Test-Path -LiteralPath $PathDst -PathType Container)) {
+    try {
+
+        # Install Chocolatery
+        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+        
+        # Clone repository 
+        git clone https://github.com/humbertocrispim/Modelos-De-Contratos.git
+ 
+    }
+    catch {
+        {"Error"}
+    }
+
+}    
 
 # Disable run script PowerShell
 powershell -command "& { Set-ExecutionPolicy Default }"
