94cc4635a45192b22de1a87dc651d9b53f1c9027
humbertocrispim <humberto.cunha.crispim@gmail.com>
2022-11-22 23:44:04 -0300

    dfe1

--- .main/main.ps1
+++ .main/main.ps1
@@ -18,6 +18,8 @@ if (-not(Test-Path -LiteralPath $PathDst -PathType Container)) {
         # Install Chocolatery
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
         
+        choco install git -y 
+
         # Clone repository 
         git clone $repository $PathDst
  
